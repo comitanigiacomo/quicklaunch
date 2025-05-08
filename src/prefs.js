ba69d03d3d4b878f83b73a3bf0394f68781b43d1
GiacomoComitani <giacomo.comitani@studenti.unimi.it>
2025-05-08 23:15:46 +0200

    fix(imports): simplify import statements for Gtk, Adw, and Gdk

--- src/prefs.js
+++ src/prefs.js
@@ -1,7 +1,7 @@
-import Gtk from 'gi://Gtk?version=4.0';
-import Adw from 'gi://Adw?version=1';
+import Gtk from 'gi://Gtk';
+import Adw from 'gi://Adw';
 import Gio from 'gi://Gio';
-import Gdk from 'gi://Gdk?version=4.0';
+import Gdk from 'gi://Gdk';
 import GLib from 'gi://GLib';
 import { ExtensionPreferences } from 'resource:///org/gnome/Shell/Extensions/js/extensions/prefs.js';
 
